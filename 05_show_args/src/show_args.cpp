b46d6d1bf837fdcf72a9355d5d5475c13e2495e3
Madalina <madalina_serban01@yahoo.com>
2022-08-02 20:31:45 +0300

    05_show_args

---
+++ 05_show_args/src/show_args.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <string>
+
+
+int main(int argc, char* argv[])
+{
+	int aux = argc;
+	int i = 1;
+	while (aux)
+	{
+		std::string argumentAsString = argv[i];
+		i++;
+		aux--;
+		std::cout << argumentAsString<<" "<<"\n";
+	}
+
+
+
+	return 0;
+}
